d3e10ab7d1ecfee1443e890e1b0061e3484a2dbb
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2022-06-02 11:44:22 +0530

    first commit

---
+++ build/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+export default configureStore({
+    reducer: {
+    
+     
+    },
+    middleware: getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
